67f5b8039f6b7d990b24c4556bf3a7301a040137
danysanches@msn.com <danysanches@msn.com>
2024-05-20 13:48:30 -0300

    SlugField

--- products/admin.py
+++ products/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product
 
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ('__str__', 'slug')
+	class meta:
+		model = Product
+
+
 admin.site.register(Product)
